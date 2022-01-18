b2110e1735c05a3b223ef6dbab1559da2432ce81
Goselm <stevengaming182@gmail.com>
2022-01-18 14:20:43 +0700

    ketinggalan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppMenuComponent } from './app-menu/app-menu.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DivisionComponent } from './division/division.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    AppMenuComponent,
+    AppHeaderComponent,
+    DashboardComponent,
+    DivisionComponent
   ],
   imports: [
     BrowserModule,
